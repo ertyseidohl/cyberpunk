abc5e3c64ce19a19440448d561547c0343d9bc57
Erty Seidel <ertysdl+github@gmail.com>
2013-10-06 23:16:58 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,376 @@
+;(function(exports){
+
+	var states = {
+		beginning: function(game, frame){
+			var anim = document.getElementById("anim");
+			if(frame == 0){
+				this.done = 0;
+				this.size = {
+					x: 100,
+					y: 100,
+					xmax: document.body.clientWidth,
+					ymax: document.body.clientHeight
+				}
+			}
+
+			this.size.x += Math.random() * 100;
+			this.size.y += Math.random() * 100;
+
+			if(this.size.x > this.size.xmax){
+				this.size.x = this.size.xmax;
+				this.done = 1;
+			}
+			if(this.size.y > this.size.ymax){
+				this.size.y = this.size.ymax;
+				if(this.done == 1) this.done = 2;
+			}
+
+			anim.style.width = this.size.x + "px";
+			anim.style.height = this.size.y + "px";
+
+			anim.style.marginTop = (-.5 * this.size.y) + "px";
+			anim.style.marginLeft = (-.5 * this.size.x) + "px";
+
+			if(this.done == 2){
+				anim.parentNode.removeChild(anim);
+				document.getElementById("anim2").parentNode.removeChild(document.getElementById("anim2"));
+				game.setState("playing");
+			}
+
+		},
+		login: function(game, frame){
+			if(frame == 0){
+				game.typewriter.push(
+					game.outEl,
+					[
+					"Please create a user for this session."
+					],
+					1,
+					function(element){
+						var s = '';
+						s += "<form id='sessionform'>"
+						s += "Username: <input type='text' id='player-name' value='debug' autocomplete='off'/>";
+						s += "<br /><br />";
+						s += "Password: <input type='password' id='player-password' value='debug' autocomplete='off'/>";
+						s += "<br /><br />";
+						s += "Click <input type='submit' value='here'/> to start this session";
+						s += "</form>";
+						game.outEl.innerHTML += s;
+						var form = document.getElementById('sessionform');
+						if (form.attachEvent) {
+							form.attachEvent("submit", createUser);
+						} else {
+							form.addEventListener("submit", createUser);
+						}
+					}
+				);
+
+			}
+		},
+		playing: function(game, frame){
+			if(game.lastScriptState == game.scriptState) return;
+
+			console.log(game.lastScriptState, " -> ", game.scriptState);
+
+			if(!script[game.scriptState]){
+				game.outEl.innerHTML = "Not Yet Written: " + game.scriptState;
+				game.setOptions({
+					1: {
+						text: "Return to last state",
+						callback: (function(lastState){
+							return function(){
+								game.lastScriptState = game.scriptState;
+								game.setScriptState(lastState);
+							}
+						})(game.lastScriptState)
+					}
+				});
+				game.lastScriptState = game.scriptState;
+				return;
+			}
+
+			if(script[game.scriptState].color){
+				game.outEl.style.color = script[game.scriptState].color;
+			} else{
+				game.outEl.style.color = "green";
+			}
+
+			if(script[game.scriptState].location){
+				game.player.location = script[game.scriptState].location;
+				game.updatePlayer();
+			}
+
+			game.typewriter.push(
+				game.outEl,
+				__(script[game.scriptState].text),
+				1,
+				function(element){
+					var i = 1;
+					var options = {};
+					script[game.scriptState].options.forEach(function(a){
+						if((a.condition && a.condition()) || !a.condition){
+							options[i] = a;
+							if(!a.callback){
+								options[i].callback = function(){
+									game.setScriptState(a.state);
+								};
+							}
+							i++;
+						}
+					});
+					this.setOptions(options);
+				}
+			);
+
+			game.lastScriptState = game.scriptState;
+
+		}
+
+	}
+
+	var TypeWriter = function(){
+		this.queue = [];
+
+		this.push = function(element, strings, delay, callback){
+			var that = this;
+			//strings = strings.map(function(s){return s.split(/(\s)/).filter(Boolean)});
+			this.queue.push({
+				"element": element,
+				"strings": strings,
+				"delay": delay,
+				delayPassed: 0,
+				stringsIndex: 0,
+				stringIndex: 0,
+				loop: that.typeFunction,
+				done: false,
+				"callback": callback
+			});
+		};
+
+		this.finish = function(){
+			this.queue.forEach(function(a){
+				a.element.innerHTML = "";
+				for(var i = 0; i < a.strings.length; i++){
+					a.element.innerHTML += a.strings[i];
+					a.element.innerHTML += "<br /><br />";
+				}
+				a.done = true;
+			});
+		}
+
+		this.typeFunction = function(){
+			if(this.done) return;
+			this.delayPassed += 1;
+			if(this.delayPassed == this.delay){
+				if(this.stringIndex == this.strings[this.stringsIndex].length){
+					this.stringIndex = 0;
+					this.stringsIndex ++;
+					this.element.innerHTML += "<br /><br />	";
+				}
+				if(this.stringsIndex == this.strings.length){
+					this.done = true;
+				} else{
+					this.element.innerHTML += this.strings[this.stringsIndex][this.stringIndex];
+					this.delayPassed = 0;
+					this.stringIndex += 1;
+				}
+			}
+		};
+
+		this.loop = function(){
+			for(var i = 0; i < this.queue.length; i++){
+				this.queue[i].loop();
+				if(this.queue[i].done){
+					if(this.queue[i].callback != undefined){
+						this.queue[i].callback.call(game, this.queue[i]);
+					}
+					this.queue.splice(i, 1);
+					i -= 1;
+				}
+			}
+		};
+	}
+
+	var __ = function(strArr){
+		return strArr.map(function(str){
+			if(typeof(str) == "function"){
+				return str.call(this);
+			}
+			return str;
+		}).filter(function(str){
+			return !!str;
+		}).map(function(str){
+			return str.replace('__NAME__', game.player.name).replace('__NAME_UC__', game.player.name.toUpperCase());
+		});
+	}
+
+	var Game = function(inId, outId, playerId, opt1Id, opt2Id, opt3Id, opt4Id){
+		this.state = states.beginning;
+
+		this.scriptState = "beginning";
+		this.lastScriptState = "";
+
+		this.inEl = document.getElementById(inId);
+		this.outEl = document.getElementById(outId);
+		this.playerEl = document.getElementById(playerId);
+
+		this.opt = {
+			1: {
+				element: document.getElementById(opt1Id),
+				callback: null,
+				enabled: false
+			},
+			2: {
+				element: document.getElementById(opt2Id),
+				callback: null,
+				enabled: false
+			},
+			3: {
+				element: document.getElementById(opt3Id),
+				callback: null,
+				enabled: false
+			},
+			4: {
+				element: document.getElementById(opt4Id),
+				callback: null,
+				enabled: false
+			}
+		}
+
+		this.player = {
+			inventory: [],
+			name: '',
+			location: '',
+			events: {}
+		};
+
+		this.items = {
+			sb2_keycard: "Mizumo Keycard"
+		}
+
+		this.updatePlayer = function(){
+			var s = '';
+			s += '<p><span class="heavy">Name:</span><br  />' + this.player.name + '</p>';
+			s += '<p><span class="heavy">Location:</span><br />' + this.player.location + '</p>';
+			s += '<p><span class="heavy">Inventory:</span><br />';
+			if(this.player.inventory.length == 0){
+				s += 'No Items';
+			} else {
+				s += '<ul>';
+				this.player.inventory.forEach(function(a){
+					s += '<li>' + a + '</li>';
+				});
+				s += '</ul>';
+			}
+			s += '</p>';
+			this.playerEl.innerHTML = s;
+		}
+
+		this.frame = 0;
+
+		this.typewriter = new TypeWriter();
+
+		this.loop = function(){
+			this.state.call(null, this, this.frame);
+			this.typewriter.loop();
+
+			this.frame += 1;
+		}
+
+		this.setScriptState = function(newState, reset){
+			if(!reset) this.reset();
+			this.scriptState = newState;
+			this.scriptStateChange = true;
+		}
+
+		this.reset = function(){
+			this.frame = -1;
+			this.outEl.innerHTML = "";
+			this.setOptions({});
+		}
+
+		this.setState = function(newState, reset){
+			if(!reset) this.reset();
+			this.state = states[newState];
+		}
+
+		this.flow = function(){
+			var height = document.body.clientHeight;
+			var inHeight = 100;
+			var outHeight = height - 20 - 20 - 10 - 10 - 4 - 40 - inHeight;
+			this.outEl.style.height = outHeight + "px";
+			this.inEl.style.height = inHeight + "px";
+			this.playerEl.style.height = outHeight + inHeight + 10 + 10 + 1 + "px";
+		};
+
+		this.setOptions = function(options){
+			options = options || {};
+			for(var i = 1; i < 5; i++){
+				if(!options[i]){
+					this.opt[i].enabled = false;
+					this.opt[i].element.style.visibility = "hidden";
+				} else{
+					this.opt[i].enabled = true;
+					this.opt[i].element.style.visibility = "";
+					document.getElementById("option" + i + "-text").innerHTML = options[i].text;
+					this.opt[i].callback = options[i].callback;
+				}
+			}
+		}
+
+		this.keyListener = function(keyCode){
+			var key = keyCode - 48; //"1"
+			if(this.opt[key] && this.opt[key].enabled && this.opt[key].callback){
+				this.opt[key].callback.call(this, this.opt[key].element);
+			} else if(key == 144){ // "~"
+				this.typewriter.finish();
+			}
+		}
+	}
+
+	var createUser = function(evt){
+		if (evt.preventDefault) evt.preventDefault();
+		var nameEl = document.getElementById("player-name");
+		var passwordEl = document.getElementById("player-password");
+		if(nameEl.value.length == 0){
+			nameEl.style.backgroundColor = "#220000";
+		}
+		if(passwordEl.value.length == 0){
+			passwordEl.style.backgroundColor = "#220000";
+		}
+		if(nameEl.value.length > 0 && passwordEl.value.length > 0){
+			game.player.name = nameEl.value;
+			game.player.password = passwordEl.value;
+			game.setScriptState("mizumo_start", false);
+			game.setState("playing");
+			game.updatePlayer();
+		}
+		return false;
+	};
+
+	exports.createUser = createUser;
+	exports.Game = Game;
+})(this);
+
+(function() {
+  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+})();
+
+var init = function(){
+	game = new Game("input", "output", "player", "option1", "option2", "option3", "option4");
+	game.setOptions({});
+	game.flow();
+	script = createScript();
+	document.onkeyup = function(evt){game.keyListener(evt.keyCode);};
+}
+
+var loop = function(){
+	game.loop();
+	window.requestAnimationFrame(loop);
+}
+requestAnimationFrame(init);
+requestAnimationFrame(loop);
+
+window.addEventListener('resize', function(event){
+  game.flow();
+});
---
+++ script.js
@@ -0,0 +1,374 @@
+var createScript = function(){return {
+	beginning:{
+		text: ["This Game is best played in fullscreen.",
+				"Most browsers access fullscreen by pressing F11.",
+				"Please adjust your browser, then hit '1' to continue.",
+				"(You can hit the tilde/backtick key to skip text animations)"],
+		options: [
+			{
+				text: "Log In",
+				callback: function(){
+					game.setState("login");
+				}
+			}
+		]
+	},
+	mizumo_start: {
+		location: 'Mizumo HQ, room 2503',
+		text: [
+			"Welcome, __NAME__",
+			"You look up from your stale coffee at the dim, glowing screen of your Mizumo Corporation terminal.",
+			"You were supposed to head home a few hours ago. Except for the occasional security guard, the rest of the Mizumo office building is dark and empty.",
+			"It's friday, but your friends cancelled their plans with you, so you figured you might as well get some last work in before the weekend."
+		],
+		options: [
+			{
+				text: "Look Around",
+				state: "beginning_looking_around"
+			}
+		]
+	},
+	beginning_looking_around: {
+		location: 'Mizumo HQ, room 2503',
+		text: [
+			"The row of offices stretches quite a ways in both directions. You have the misfortune of inhabiting the office right in the middle of this floor.",
+			"You have a small light on in your office, casting a bluish glow across your desk.",
+			"Mizumo Corporation is a world renowned Computing and Simulation corporation - and you spend all your time writing stupid SEO stuff for the website. Someone's gotta do it.",
+			"An alert flashes up on your computer terminal."
+		],
+		options: [
+			{
+				text: "Examime Alert",
+				state: "mizumo_examine_alert"
+			},
+			{
+				text: "Ingore Alert",
+				state: "mizumo_leaving"
+			}
+		]
+	},
+	mizumo_examine_alert: {
+		location: 'Mizumo HQ, room 2503',
+		color: "#0000ff",
+		text: [
+			"To: __NAME_UC__",
+			"From: JANUS",
+			"Subject: HELP PLEASE",
+			"I'M TRAPPED IN ROOM 0029 IN SUB-BASEMENT 2. IS ANYONE STILL AROUND? I'VE TRIED CALLING SECURITY BUT THEY AREN'T ANSWERING.",
+			"I KNOW THIS IS TOTALLY AGAINST POLICY, BUT THERE'S A SPARE KEYCARD IN MY DESK (ROOM 2206) IN THE THIRD DRAWER DOWN THAT WILL GET YOU INTO THE BASEMENT.",
+			"I'VE BEEN DOWN HERE A LONG TIME ALREADY AND I REALLY DON'T WANT TO BE HERE UNTIL MONDAY!",
+			"PLEASE HELP ME!",
+			"JANUS"
+		],
+		options: [
+			{
+				text: "Go to Room 2206",
+				state: "mizumo_room_2206"
+			},
+			{
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			},
+			{
+				text: "Ignore Message",
+				state: "mizumo_leaving"
+			},
+			{
+				condition: function(){return game.player.events["mizumo_opsec_line_dead"] == undefined},
+				text: "Make OpSec deal with it",
+				state: "mizumo_call_opsec"
+			},
+			{
+				condition: function(){return game.player.events["mizumo_opsec_line_dead"] == true},
+				text: "Call the Opsec office again",
+				state: "mizumo_call_opsec_dead"
+			}
+		]
+	},
+	mizumo_room_2206: {
+		location: 'Mizumo HQ, room 2206',
+		text: [
+			"Room 2206 is an office on the 22nd floor - three floors down from you. It's a smaller office than yours, for what that's worth.",
+			"You have an uneasy feeling, but it's probably becuase most of the lights are off here and there's nobody around."
+		],
+		options: [
+			{
+				condition: function(){return game.player.events["mizumo_2206_snooped"] == undefined},
+				text: "Look Around",
+				callback: function(){
+					game.player.events["mizumo_2206_snooped"] = true;
+					game.setScriptState("mizumo_room_2206_examine");
+				}
+			},
+			{
+				condition: function(){return game.player.inventory.indexOf(game.items.sb2_keycard) == -1},
+				text: "Get Keycard",
+				callback: function(){
+					game.player.inventory.push(game.items.sb2_keycard);
+					game.updatePlayer();
+					game.setScriptState("mizumo_room_2206_keycard");
+				}
+			},
+			{
+				condition: function(){return game.player.events["mizumo_2206_snooped"] == undefined},
+				text: "Snoop around in the office",
+				callback: function(){
+					game.player.events["mizumo_2206_snooped"] = true;
+					game.setScriptState("mizumo_room_2206_examine");
+				}
+			},
+			{
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			}
+		]
+	},
+	mizumo_room_2206_examine: {
+		location: 'Mizumo HQ, room 2206',
+		text: [
+			"You hear the small whine of something mechanical behind you, and you whip around. You see a security camera zooming in on you. Idiots in Opsec have time to watch you on the camera but not save people in the basement.",
+			function(){if(!game.player.events["mizumo_opsec_line_dead"]) return "Come to think of it, you could probably just call Opsec and have them rescue James or whoever they were."},
+			"You notice that this room seems... almost too empty for someone to be working in it. There's barely anything on the desk. Either way, it looks like the keycard should be where they said."
+		],
+		options: [
+			{
+				condition: function(){return game.player.inventory.indexOf(game.items.sb2_keycard) == -1},
+				text: "Get Keycard",
+				callback: function(){
+					game.player.inventory.push(game.items.sb2_keycard);
+					game.updatePlayer();
+					game.setScriptState("mizumo_room_2206_keycard");
+				}
+			},
+			{
+				condition: function(){return game.player.events["mizumo_opsec_line_dead"] == undefined},
+				text: "Call the Opsec office",
+				state: "mizumo_call_opsec"
+			},
+			{
+				condition: function(){return game.player.events["mizumo_opsec_line_dead"] == true},
+				text: "Call the Opsec office",
+				state: "mizumo_call_opsec_dead"
+			},
+			{
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			}
+		]
+	},
+	mizumo_call_opsec: {
+		location: undefined,
+		text: [
+			"\"Hello, Mizumo Opsec.\""
+		],
+		color: "red",
+		options: [
+			{
+				text: "Tell them about the person in the basement",
+				callback: function(){
+					game.setScriptState("mizumo_call_opsec_tell");
+					game.player.events["mizumo_opsec_line_dead"] = true;
+				}
+			},
+			{
+				text: "Nevermind",
+				state: "mizumo_call_opsec_end"
+			}
+		]
+	},
+	mizumo_call_opsec_tell: {
+		location: undefined,
+		text: [
+			"You're about to start explaining, when the line suddenly goes dead. Weird."
+		],
+		options: [
+			{
+				text: "Call opsec again.",
+				state: "mizumo_call_opsec_dead"
+			},
+			{
+				text: "Too creepy - give up and go home",
+				state: "mizumo_leaving"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) != -1},
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) == -1},
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) == -1},
+				text: "Get Keycard from Desk",
+				callback: function(){
+					game.player.inventory.push(game.items.sb2_keycard);
+					game.updatePlayer();
+					game.setScriptState("mizumo_room_2206_keycard");
+				}
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2503"},
+				//player is in their own office
+				text: "Go to Room 2206",
+				state: "mizumo_room_2206"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2503"},
+				//player is in their own office
+				text: "Ignore the message - go home",
+				state: "mizumo_leaving"
+			}
+		]
+	},
+	mizumo_call_opsec_end:{
+		location: undefined,
+		text: [
+			"\"Okay, thank you for calling.\""
+		],
+		color: "red",
+		options: [
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) == -1},
+				//player is in downstairs office, no keycard
+				text: "Get Keycard from Desk",
+				callback: function(){
+					game.player.inventory.push(game.items.sb2_keycard);
+					game.updatePlayer();
+					game.setScriptState("mizumo_room_2206_keycard");
+				}
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) == -1},
+				//player is in downstairs office, no keycard
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) != -1},
+				//player is in downstairs office, keycard
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) != -1},
+				//player is in downstairs office, keycard
+				text: "Not worth losing your job",
+				state: "mizumo_leaving"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2503"},
+				//player is in their own office
+				text: "Go to Room 2206",
+				state: "mizumo_room_2206"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2503"},
+				//player is in their own office
+				text: "Ignore the message - go home",
+				state: "mizumo_leaving"
+			}
+		]
+
+	},
+	mizumo_call_opsec_dead: {
+		location: undefined,
+		text: [
+			"You pick up the phone, but there's no dial tone."
+		],
+		options: [
+			{
+				text: "Too creepy - give up and go home",
+				state: "mizumo_leaving"
+			},
+			{
+				text: "Go to Elevator",
+				state: "mizumo_elevator_locked"
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2206" && game.player.inventory.indexOf(game.items.sb2_keycard) == -1},
+				text: "Get Keycard from Desk",
+				callback: function(){
+					game.player.inventory.push(game.items.sb2_keycard);
+					game.updatePlayer();
+					game.setScriptState("mizumo_room_2206_keycard");
+				}
+			},
+			{
+				condition: function(){return game.player.location == "Mizumo HQ, room 2503"},
+				//player is in their own office
+				text: "Go to Room 2206",
+				state: "mizumo_room_2206"
+			}
+		]
+	},
+	mizumo_room_2206_keycard: {
+		location: 'Mizumo HQ, room 2206',
+		text: [
+			"You pick up the keycard. It looks like your own, except that their department code is \"AIR\", which you've never heard of. The name on the keycard is \"Jason Landes\".",
+			"The sub-basements have always been an in-joke at Mizumo, that there's strange research happening there, but you've always assumed it was just poking fun at the corporation.",
+			"Well, now might be your chance to find out."
+		],
+		options: [
+			{
+				text: "Not worth losing your job",
+				state: "mizumo_leaving"
+			},
+			{
+				text: "Go help the trapped person",
+				state: "mizumo_elevator_locked"
+			}
+		]
+	},
+	mizumo_elevator_locked: {
+		location: 'Mizumo HQ, Service Elevator 07b',
+		text: [
+			"You step into the elevator. It looks like there are three sub basements, but there are small lock icons near them, and the elevator doesn't respond if you press them.",
+		],
+		options: [
+			{
+				text: "Return to Room 2206",
+				state: "mizumo_room_2206"
+			},
+			{
+				condition: function(){return game.player.inventory.indexOf(game.items.sb2_keycard) != -1},
+				text: "Use Mizumo Keycard",
+				state: "mizumo_elevator_unlocked"
+			}
+		]
+	},
+	mizumo_elevator_unlocked: {
+		location: 'Mizumo HQ, Service Elevator 07b',
+		text: [
+			"The lock icon on the button for sub-basement 2 disappears."
+		],
+		options: [
+			{
+				text: "Return to Room 2206",
+				state: "mizumo_room_2206"
+			},
+			{
+				text: "Go to Sub-Basement 2",
+				state: "mizumo_sb2_arrive"
+			},
+			{
+				text: "Last chance to wimp out",
+				state: "mizumo_leaving"
+			}
+		]
+	},
+	mizumo_leaving: {
+		location: 'Mizumo Front Lobby',
+		text: [
+			"You leave."
+		],
+		options: [
+			{
+				text: "On second thought...",
+				state: "mizumo_room_2206"
+			}
+		]
+	}
+}};
